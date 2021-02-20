dd88dd5e3d226de1301d4a1f0fba620f2f195aea
Max Simmonds <maxsimmonds1337@gmail.com>
2021-02-20 13:27:58 +0000

    interupts working, motor is even spinning

--- main.c
+++ main.c
@@ -11,6 +11,25 @@ void LCD_Write(unsigned char DATA, unsigned char command);
 void ADC_EN(void);
 
 
+const int hall_counts_per_rot = 23; // number of counts per one wheel rotation
+const float wheel_diameter = 1;//  0.662; //meters
+const float pi = 3.1412; // pi
+const float cir = pi * wheel_diameter; // circumfrenece of the wheel
+const float mps_mph = 2.23694; // m/s to m/h conversion
+
+//define some vars
+volatile unsigned char LED_ON; // this is used to toggle the LED
+volatile unsigned int HAL_count, HAL_count_frozen, timer_count = 0; // used for counting
+
+int Vin, Vin_largest,adc_reg = 0; // this reads the input voltage
+
+
+
+// used for delay
+int a,D = 0;
+
+double speed, speed_largest,speed_constant = 0.0; // var to hold the speed
+
 #define throttle_scale 0.80586  // 12 bit adc, 3v3 is full scale
 
 struct IO_Expander {
@@ -29,15 +48,15 @@ char slave_add = 0x20;
 char display_str[16];
 static const struct IO_Expander IO_Clear;
 
-int speed = 0;
 float throttle = 0.0;
 
 int digits[4]; // this will store the digits
+volatile int duty_cycle = 0;
 
 //main loop
 int main(void){
 	
-	volatile int i, val, duty_cycle = 0;
+	volatile int i, val = 0;
 	
 	delay(400); //wait 15ms
 	
@@ -85,6 +104,33 @@ int main(void){
 	LCD_Write(0x0C,1);
 	LCD_Write(0x06,1);
 	
+	/**** TIM3 INIT ******/
+	RCC->APB1ENR |= RCC_APB1ENR_TIM3EN; // enable the timer
+	TIM3->PSC = 1000; 
+	TIM3->ARR = 1000; //125ms delay
+	TIM3->DIER |= TIM_DIER_UIE; //enable timer interrupts
+	NVIC_EnableIRQ(TIM3_IRQn); // enable the IRQ
+	TIM3->CR1 |= TIM_CR1_CEN; //enable the counter
+	
+	/**** TIM6 INIT ******/
+	RCC->APB1ENR |= RCC_APB1ENR_TIM14EN; // enable the timer
+	TIM14->PSC = 24; 
+	TIM14->DIER |= TIM_DIER_UIE; //enable timer interrupts
+	NVIC_EnableIRQ(TIM14_IRQn); // enable the IRQ
+	TIM14->CR1 |= TIM_CR1_CEN; //enable the counter
+	
+	/****** INT INIT ********/
+	//EXTI is on syscfg clk, so needs to be enabled
+	RCC->AHBENR |= RCC_AHBENR_GPIOBEN; // enable port b
+	RCC->APB2ENR |= RCC_APB2ENR_SYSCFGEN;
+	SYSCFG->EXTICR[0] |= SYSCFG_EXTICR1_EXTI0_PB;	//link line 0 with Port B (so we're using PB0 as the interrupt)
+	
+	//Configure the interupts
+	EXTI->IMR |= EXTI_IMR_MR0; // set line 0 to be enabled
+	EXTI->RTSR	|= EXTI_RTSR_TR0; // set it to trigger on a rising edge
+	
+	NVIC_SetPriority(EXTI0_1_IRQn, 0x03);
+	NVIC_EnableIRQ(EXTI0_1_IRQn);
 	
 	
 	
@@ -110,38 +156,29 @@ int main(void){
 
 	strcpy(display_str, "Throttle = ");
 	LCD_write_str(display_str);
-
+	
+	
 	while(1){
 		
+		delay(10);
 	
 		LCD_Write(0x86, 0x1); // set the ddram address to row 2 col 1
-	  
-		if(speed == 9){
-			speed = 0;
-		} else {
-			speed++;
+		speed = ((HAL_count_frozen * cir/hall_counts_per_rot) / 0.5) * mps_mph; // calculate the speed, in mph
+	
+		// TODO make this a function
+		for(i = 0; i < 2; i++) {
+			digits[i] = (int)speed % 10;
+			speed /= 10;
 		}
-	  
-		//LCD_Write(0x6, 0x1); // set the ddram address to row 2 col 1
-		LCD_Write('0' + speed, 0x0); // turn off LED
 		
-		ADC1->CR |= ADC_CR_ADSTART;
+		LCD_Write('0' + digits[1], 0x0);
+		LCD_Write('0' + digits[0], 0x0);
 		
-		while ((ADC1->ISR & ADC_ISR_EOC) == 0) /* Wait end of conversion */
-		{
- /* For robust implementation, add here time-out management */
-		}
-		throttle = ADC1->DR;
+		strcpy(display_str, " MPH");
+		LCD_write_str(display_str);
 		
 		LCD_Write(0xC0+0xB, 0x1); // set the ddram address to row 2 col 1
-				
-		throttle /= (4095.0);
-		throttle *= 100.0;
-		
-		duty_cycle = 405 - 4*(unsigned int)(throttle); // set duty cycle
-
-		TIM1->CCR4 = duty_cycle;
-		
+		throttle /= 4;
 		for(i = 0; i < 3; i++) {
 			digits[i] = (int)throttle % 10;
 			throttle /= 10;
@@ -265,6 +302,45 @@ while ((ADC1->ISR & ADC_ISR_ADRDY) == 0) /* (4) */
  /* For robust implementation, add here time-out management */
 }
 	
+}
+
+//interrupt service routine for the timer
+int TIM3_IRQHandler(void) {
+  TIM3->SR = 0;		// clear the status reg
+	// start ADC acq.
+	ADC1->CR |= ADC_CR_ADSTART;
+	while ((ADC1->ISR & ADC_ISR_EOC) == 0); // Wait end of conversion
+	
+	// read the ADC DR and return
+	throttle = ADC1->DR;
+	throttle /= (4095.0);
+	throttle *= 400.0;
+	duty_cycle = 405 - (unsigned int)(throttle); // set duty cycle
+	TIM1->CCR4 = duty_cycle;
+	
+	return throttle;
+}
+
+//interrupt service routine for the timer
+int TIM14_IRQHandler(void) {
+  TIM14->SR = 0;		// clear the status reg
+	
+	HAL_count_frozen = HAL_count; // freeze the counter
+	HAL_count = 0;// reset the counter
+	
+	return HAL_count_frozen; // return the value for processing outside the IRQ
+}
+
+//interrupt service routine, for counting pulses
+void EXTI0_1_IRQHandler(void) {
+	if(EXTI->PR & (GPIOB->IDR) ) {
+		//clear the progress reg
+		EXTI->PR = 0x1;
+		
+		//LED_ON = !LED_ON; // toggle the led
+		
+		HAL_count++; // inc the counter
+	}
 }
 
 //throttle *= throttle_scale;
